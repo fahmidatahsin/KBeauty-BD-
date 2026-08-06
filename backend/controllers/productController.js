b72fa70c9aed5daf73fee5aa27aea284714634a3
Fahmida Tahsin <ft7389556@gmail.com>
2026-08-06 13:09:12 +0600

    Implement get all products API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -24,6 +24,11 @@ res.status(201).json({
     product,
 });
 };
+const getAllProducts = async (req, res) => {
+    const products = await Product.find();
+res.status(200).json(products);
+};
 module.exports = {
     addProduct,
+    getAllProducts,
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { addProduct } = require("../controllers/productController");
+const {
+    addProduct,
+    getAllProducts,
+} = require("../controllers/productController");
+router.get("/", getAllProducts);
 router.post("/", addProduct);
 module.exports = router;
