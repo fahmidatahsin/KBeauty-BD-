5ecb00b932c4daec3842ad30c54ec62ac32c13a4
Fahmida Tahsin <ft7389556@gmail.com>
2026-08-06 14:33:05 +0600

    Implement add category API

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 const app = express();
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
 
 app.get("/", (req, res) => {
     res.send("Server is running...");
