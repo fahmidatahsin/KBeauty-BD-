ff6fa3cf55076d6ce6c7d9d04fb1cc53b1de9e3a
Fahmida Tahsin <ft7389556@gmail.com>
2026-08-07 14:27:14 +0600

    Implement category CRUD APIs

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -16,6 +16,53 @@ res.status(201).json({
     category,
 });
 };
+const getAllCategories = async (req, res) => {
+const categories = await Category.find();
+res.status(200).json(categories);
+};
+const getSingleCategory = async (req, res) => {
+const { id } = req.params;
+const category = await Category.findById(id);
+if (!category) {
+    return res.status(404).json({
+        message: "Category not found",
+    });
+}
+res.status(200).json(category);
+};
+const updateCategory = async (req, res) => {
+const { id } = req.params;
+const { name } = req.body;
+const category = await Category.findById(id);
+if (!category) {
+    return res.status(404).json({
+        message: "Category not found",
+    });
+}
+category.name = name;
+await category.save();
+res.status(200).json({
+    message: "Category updated successfully",
+    category,
+});
+};
+const deleteCategory = async (req, res) => {
+const { id } = req.params;
+const category = await Category.findById(id);
+if (!category) {
+    return res.status(404).json({
+        message: "Category not found",
+    });
+}
+await category.deleteOne();
+res.status(200).json({
+    message: "Category deleted successfully",
+});
+};    
 module.exports = {
     addCategory,
+    getAllCategories,
+    getSingleCategory,
+    updateCategory,
+    deleteCategory,
 };
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -3,8 +3,14 @@ const router = express.Router();
 
 const {
     addCategory,
+    getAllCategories,
+    getSingleCategory,
+    updateCategory,
+    deleteCategory,
 } = require("../controllers/categoryController");
-
+router.get("/", getAllCategories);
 router.post("/", addCategory);
-
+router.get("/:id", getSingleCategory);
+router.put("/:id", updateCategory);
+router.delete("/:id", deleteCategory);
 module.exports = router;
